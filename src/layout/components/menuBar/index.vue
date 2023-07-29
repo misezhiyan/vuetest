63eeeb543229861b0313007fe4f535f7de6984ff
kimmy <misezhiyan@163.com>
2023-07-29 10:08:07 +0800

    子菜单

--- src/layout/components/menuBar/index.vue
+++ src/layout/components/menuBar/index.vue
@@ -1,44 +1,79 @@
 <template>
-  <!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
-      <el-radio-button :label="false">expand</el-radio-button>
-      <el-radio-button :label="true">collapse</el-radio-button>
-    </el-radio-group> -->
   <el-menu default-active="1" active-text-color="#409EFF" text-color="#FFF" class="el-menu" :collapse="collapsed">
-    <el-sub-menu index="1">
-      <template #title>
-        <el-icon>
-          <location />
-        </el-icon>
-        <span>Navigator One</span>
-      </template>
-      <el-menu-item-group>
-        <template #title><span>Group One</span></template>
-        <el-menu-item index="1-1">item one</el-menu-item>
-        <el-menu-item index="1-2">item two</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="Group Two">
-        <el-menu-item index="1-3">item three</el-menu-item>
-      </el-menu-item-group>
-      <el-sub-menu index="1-4">
-        <template #title><span>item four</span></template>
-        <el-menu-item index="1-4-1">item one</el-menu-item>
-      </el-sub-menu>
-    </el-sub-menu>
-    <el-menu-item index="2">
-      <el-icon>
-        <Menu />
-      </el-icon>
-      <template #title>Navigator Two</template>
-    </el-menu-item>
+    <menuItem :menus="menus"/>
   </el-menu>
 </template>
   
 <script lang="ts" setup>
+import { reactive } from 'vue';
+import menuItem from './menuItem.vue';
+
 defineProps({
   collapsed: {
     type: Boolean
   }
 })
+
+const menus = reactive(
+  [{
+    "path": "/",
+    "redirect": "/index",
+    "name": "Index",
+    "meta": {
+      title: '首页',
+      icon: 'house'
+    },
+    "children": [{
+      "path": "/index",
+      "name": "Index",
+      "meta": {
+        title: '首页',
+        icon: 'house'
+      },
+    }]
+  }, {
+    "path": "/user",
+    "redirect": "/user",
+    "name": "User",
+    "meta": {
+      title: '用户管理',
+      affix: true,
+      icon: 'UserFilled'
+    },
+    "children": [{
+      "path": "/manager",
+      "name": "UserManager",
+      "meta": {
+        title: '用户管理',
+        icon: 'UserFilled'
+      },
+    }]
+  }, {
+    "path": "/db",
+    "redirect": "/db",
+    "name": "Db",
+    "meta": {
+      title: '数据库',
+      affix: true,
+      icon: 'UserFilled'
+    },
+    "children": [{
+      "path": "/index",
+      "name": "DbIndex",
+      "meta": {
+        title: '数据库列表',
+        icon: 'UserFilled'
+      },
+    }, {
+      "path": "/dbConfig",
+      "name": "DbIndex",
+      "meta": {
+        title: '数据库配置',
+        icon: 'UserFilled'
+      },
+    }]
+  }])
+
 </script>
   
 <style lang="scss">
---
+++ src/layout/components/menuBar/menuItem.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <template v-for="menu in menus" :key="menu.path">
+        <el-sub-menu v-if="menu.children && menu.children.length > 1" index="1">
+            <template #title>
+                <el-icon>
+                    <component :is="menu.meta?.icon" />
+                </el-icon>
+                <span>{{ menu.meta?.title }}</span>
+            </template>
+            <menuItem :menus="menu.children" />
+        </el-sub-menu>
+
+        <el-menu-item v-else index="2">
+            <el-icon>
+                <component :is="menu.meta?.icon" />
+            </el-icon>
+            <span>{{ menu.meta?.title }}</span>
+        </el-menu-item>
+    </template>
+</template>
+
+
+<script setup lang="ts">
+defineProps(['menus'])
+</script>
+
